1856bb3af7a1bb4ad817c7595bcf0109f11b51ca
Tanmay Mishu <tanmaymishu@gmail.com>
2024-04-24 11:26:04 +0600

    WIP

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -21,6 +21,6 @@ return {
       },
     })
     vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>", {})
-    vim.keymap.set("n", "<leader>r", ":Neotree reveal<CR>", {})
+    vim.keymap.set("n", "<leader>nn", ":Neotree reveal<CR>", {})
   end,
 }
