4e0c7f0661b9b01a06f2e7f2478971e62615fb2a
Tanmay Das <tanmaymishu@gmail.com>
2024-06-10 17:03:37 +0600

    WIP

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,4 +12,8 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("vim-options")
 require("keymaps")
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
