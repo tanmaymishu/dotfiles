88b6060ddc8d8884235ef9ca290c942ccf6693e9
Tanmay Mishu <tanmaymishu@gmail.com>
2024-06-21 14:58:25 +0600

    Comment php_only and blade parser temporarily

--- nvim/lua/plugins/astro.lua
+++ nvim/lua/plugins/astro.lua
@@ -1,7 +1,7 @@
 return {
-	'wbthomason/vim-astro',
-	lazy = false, -- make sure to load this plugin for syntax highlighting to work
-	config = function()
-		-- put any additional configuration options here
-	end,
+  'wuelnerdotexe/vim-astro',
+  lazy = false, -- make sure to load this plugin for syntax highlighting to work
+  config = function()
+    -- put any additional configuration options here
+  end,
 }
