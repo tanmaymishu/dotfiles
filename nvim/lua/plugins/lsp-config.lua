c71b6b689bd4128b01d7b3640b0aae6e6c278e3d
Tanmay Das <tanmaymishu@gmail.com>
2024-06-29 02:10:00 +0600

    WIP

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -59,8 +59,47 @@ return {
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
+      -- local util = require 'lspconfig.util'
+      -- local function get_typescript_server_path(root_dir)
+      --   local global_ts = '/opt/homebrew/lib/node_modules/typescript/lib'
+      --   -- Alternative location if installed as root:
+      --   -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
+      --   local found_ts = ''
+      --   local function check_dir(path)
+      --     found_ts = util.path.join(path, 'node_modules', 'typescript', 'lib')
+      --     if util.path.exists(found_ts) then
+      --       return path
+      --     end
+      --   end
+      --   if util.search_ancestors(root_dir, check_dir) then
+      --     return found_ts
+      --   else
+      --     return global_ts
+      --   end
+      -- end
+
+      -- lspconfig.volar.setup {
+      --   on_new_config = function(new_config, new_root_dir)
+      --     new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+      --   end,
+      --   filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' }
+      -- }
+      -- lspconfig.volar.setup {
+      --   filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' }
+      -- }
       lspconfig.tsserver.setup({
         capabilities = capabilities,
+        on_attach = function(_client, buffer)
+          local function goto_source_definition()
+            local position_params = vim.lsp.util.make_position_params()
+            vim.lsp.buf.execute_command({
+              command = "_typescript.goToSourceDefinition",
+              arguments = { vim.api.nvim_buf_get_name(0), position_params.position },
+            })
+          end
+          local opts = { buffer = buffer }
+          vim.keymap.set("n", "<leader>gz", goto_source_definition, opts)
+        end,
         handlers = {
           ['textDocument/definition'] = function(err, result, method, ...)
             if vim.tbl_islist(result) and #result > 1 then
@@ -69,7 +108,16 @@ return {
             end
 
             vim.lsp.handlers['textDocument/definition'](err, result, method, ...)
-          end
+          end,
+          ["workspace/executeCommand"] = function(_err, result, ctx, _config)
+            if ctx.params.command ~= "_typescript.goToSourceDefinition" then
+              return
+            end
+            if result == nil or #result == 0 then
+              return
+            end
+            vim.lsp.util.jump_to_location(result[1], "utf-8")
+          end,
         }
       })
       lspconfig.tailwindcss.setup({
