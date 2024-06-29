7dd79e4acfbb9d19a1564f7a643449fd5267fd06
Tanmay Das <tanmaymishu@gmail.com>
2024-06-29 16:36:57 +0600

    WIP

--- nvim/lua/plugins/persistence.lua
+++ nvim/lua/plugins/persistence.lua
@@ -4,12 +4,12 @@ return {
 	config = function()
 		require("persistence").setup()
 		-- restore the session for the current directory
-		vim.api.nvim_set_keymap("n", "<leader>qs", [[<cmd>lua require("persistence").load()<cr>]], {})
+		vim.api.nvim_set_keymap("n", "<leader>ps", [[<cmd>lua require("persistence").load()<cr>]], {})
 
 		-- restore the last session
-		vim.api.nvim_set_keymap("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<cr>]], {})
+		vim.api.nvim_set_keymap("n", "<leader>pl", [[<cmd>lua require("persistence").load({ last = true })<cr>]], {})
 
 		-- stop Persistence => session won't be saved on exit
-		vim.api.nvim_set_keymap("n", "<leader>qd", [[<cmd>lua require("persistence").stop()<cr>]], {})
+		vim.api.nvim_set_keymap("n", "<leader>pd", [[<cmd>lua require("persistence").stop()<cr>]], {})
 	end,
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,6 +13,26 @@ return {
 		},
 		config = function()
 			local telescope = require("telescope")
+			local conf = require("telescope.config").values
+			local harpoon = require('harpoon')
+			harpoon:setup({})
+			local function toggle_telescope(harpoon_files)
+				local file_paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(file_paths, item.value)
+				end
+
+				require("telescope.pickers").new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({
+						results = file_paths,
+					}),
+					previewer = conf.file_previewer({}),
+					sorter = conf.generic_sorter({}),
+				}):find()
+			end
+			vim.keymap.set("n", "<leader>lh", function() toggle_telescope(harpoon:list()) end,
+				{ desc = "Open harpoon window" })
 			local builtin = require("telescope.builtin")
 			vim.api.nvim_create_autocmd("FileType", {
 				pattern = "TelescopeResults",
